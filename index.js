e99702aef794112dfe98ce13b1f08b1d3feb855a
Solomon <olurebisolomon@gmail.com>
2025-09-23 06:50:50 -0700

    added the confrm order section

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import { products } from "./product.js";
 let cartContainer = document.getElementById("cart-display");
 let itemsInCart = document.getElementById("cartquantity");
 let totalPrice = document.getElementById("total")
+let confirmationContainer = document.querySelector('#confirmationContainer')
+let confirmationPrice =  document.getElementById("confirmationTotal")
 
 let cart = [
     ]
@@ -30,7 +32,7 @@ let html = "";   //   declared where the products will be stored as it is looped
                     <img src="./assets/images/icon-increment-quantity.svg" alt="plus sign" class="border border-white rounded-[50%] py-1 px-[5px] border-2 cursor-pointer increase atc" data-product-name = "${product.name}" data-price="${product.price}">
                 </div>
 
-                <div class="flex justify-center gap-2 max-w-max bg-white border border-rose-900 px-6 py-2 rounded-full cursor-pointer hover:shadow-lg atc original"  data-product-name = "${product.name}" data-price="${product.price}">
+                <div class="flex justify-center gap-2 max-w-max bg-white border border-rose-900 px-6 py-2 rounded-full cursor-pointer hover:shadow-lg atc original"  data-product-name = "${product.name}" data-price="${product.price}" data-image= "${product.image.thumbnail}">
                     <img src="./assets/images/icon-add-to-cart.svg" alt="add to cart icon">
                     <button">Add to Cart</button>
                 </div>
@@ -98,12 +100,14 @@ let html = "";   //   declared where the products will be stored as it is looped
         atc.addEventListener('click', () =>{
             const productName = atc.dataset.productName;
             const cartItemPrice = Number(atc.dataset.price);
+            const cartImage = atc.dataset.image
             
             // this.classList.add('hidden')
             // console.log()
 
-            addToCart(productName,cartItemPrice);
+            addToCart(productName,cartItemPrice,cartImage);
             loadCart();
+            confirmOrder();
             console.log(cart)            
             // 
             // console.log(matchingItem)
@@ -114,9 +118,10 @@ let html = "";   //   declared where the products will be stored as it is looped
     document.querySelectorAll('.decrease').forEach(decreaseButton => {
         decreaseButton.addEventListener('click' , () => {
             // console.log('clicked')
-            const productName = decreaseButton.dataset.productName
+            const productName = decreaseButton.dataset.productName;
 
-            reduceFromCart(productName)
+            reduceFromCart(productName);
+            confirmOrder();
              loadCart();
             console.log(cart)  
         })
@@ -125,7 +130,7 @@ let html = "";   //   declared where the products will be stored as it is looped
     
     cartContainer.addEventListener('click', (e) => {
         const btn = e.target.closest('.removeButton')
-        const productName = btn.dataset.productName
+        const productName = btn.dataset.productName;
         console.log(btn);
         console.log(productName);
         if (btn){
@@ -139,7 +144,7 @@ let html = "";   //   declared where the products will be stored as it is looped
         }
     );
 
-    function addToCart(productName,cartItemPrice) {
+    function addToCart(productName,cartItemPrice, cartImage) {
         let matchingItem;
         cart.forEach((cartItem) => {
             if(productName === cartItem.name){
@@ -160,7 +165,8 @@ let html = "";   //   declared where the products will be stored as it is looped
             cart.push({
             "name": productName,
             "price": cartItemPrice,
-            "quantity": 1
+            "quantity": 1,
+            "image": {"thumbnail": cartImage}
             })
             document.querySelectorAll('.itemnumber').forEach(span => {
              if (span.dataset.productName === productName) {
@@ -261,3 +267,74 @@ let html = "";   //   declared where the products will be stored as it is looped
     // You’d also add the item to your cart array here
   });
 });
+
+function confirmOrder() {
+    let confirmationHtml = '';
+    cart.forEach(cartItem => {
+        confirmationHtml += ` <div id="orderConfirmation" class="flex justify-between items-center  border-b-[1px] pb-[15px] border-[#c83b0e]/5 mb-[30px]">
+
+            <div class="flex gap-[20px]">
+              <div class="w-[60px]">
+                <img src="${cartItem.image.thumbnail}">
+              </div>
+              <div class="flex flex-col justify-between">
+                <p class="font-bold text-[#1f1108]">${cartItem.name}</p>
+                <div class="flex gap-[15px]">
+                  <p class="text-[#c63b0e] font-bold">${cartItem.quantity}x</p>
+                  <p class="text-[#a09594] font-medium">@ #${cartItem.price}</p>
+                </div>
+              </div>
+            </div>
+
+            <p class="text-[#1f1108] font-bold text-[1.1rem]">#${(cartItem.price.toFixed(2) * cartItem.quantity).toFixed(2)}</p>
+          </div>`
+    })
+   confirmationContainer.innerHTML = confirmationHtml
+   updateCart()
+}
+
+document.getElementById('confirm').addEventListener('click',() => {
+    document.getElementById('orderConfirmationSection').classList.remove('hidden')
+})
+
+document.getElementById('startNewOrder').addEventListener('click', () =>{
+    document.getElementById('orderConfirmationSection').classList.add('hidden')
+    window.location.reload()
+})
+
+// console.log(confirmationHtml)
+
+
+
+//   <section>
+//     <div class="absolute top-[20%] left-[35%] bg-white w-[500px] h-fit p-[30px] rounded-[15px]">
+//       <img src="./assets/images/icon-order-confirmed.svg" alt="icon-order-confirmed">
+//       <h2 class="font-bold text-[2rem]">Order Confirmed</h2>
+//       <p class="text-[#b1a6a4]">We hope you enjoy your food!</p>
+
+//       <div class="bg-[#fcf8f5] p-[20px] rounded-[15px] my-[30px] ">
+//         <div id="orderConfirmation" class="flex justify-between items-center  border-b-[1px] pb-[15px] border-[#c83b0e]/5 mb-[30px]">
+
+//           <div class="flex gap-[20px]">
+//             <div class="w-[60px]">
+//               <img src="./assets/images/image-baklava-thumbnail.jpg" alt="image-baklava-thumbnail">
+//             </div>
+//             <div class="flex flex-col justify-between">
+//               <p class="font-bold text-[#1f1108]">Classic Tiramusu</p>
+//               <div class="flex gap-[15px]">
+//                 <p class="text-[#c63b0e] font-bold">1x</p>
+//                 <p class="text-[#a09594] font-medium">@ #5.50</p>
+//               </div>
+//             </div>
+//           </div>
+
+//           <p class="text-[#1f1108] font-bold text-[1.1rem]">#5.50</p>
+//         </div>
+//         <div class="flex justify-between  items-center">
+//           <p>Order Total</p>
+//           <p class="font-bold text-[1.5rem] text-[#1f1108]">#46.50</p>
+//         </div>
+//       </div>
+//       <button class="cursor-pointer w-full bg-[#c63b0e] text-white rounded-[30px] p-[10px]">Start New Order</button>
+//     </div>
+//   </section>
